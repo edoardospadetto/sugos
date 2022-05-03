dfb5cbfb2c0edf18d817150802dc587a59ad310e
edoardospadetto <edoardo.spadetto1@gmail.com>
2022-05-04 01:36:16 +0200

    Revert "Revert "geometry misc""
    This reverts commit eca767e526af84bf5836c714e1f710e78f664f55.

--- src/geometrymisc.cpp
+++ src/geometrymisc.cpp
@@ -140,3 +140,76 @@ void GenTriangle(VectorizedObject& obj)
 
 
 }
+
+
+
+
+/*
+
+void GenColor(float* target, float r ,float g, float b)
+{
+	size_t vertices = sizeof(target)/(3*sizeof(target[0]));
+	
+	for( int i =0; i< vertices, i++  )
+	{
+		target[3*i+0] = r;
+		target[3*i+1] = g;
+		target[3*i+2] = b; 
+	}	
+}
+
+GenTriangleObj( VectorizedObject& obj, float x, float y, float z )
+{
+
+	
+	for (int i =0; i<6; i++)
+	{
+		obj.vertex_buffer[i] = vertex[i];
+	}
+	
+	for (int i =0; i<3; i++)
+	{
+		obj.index_buffer[i]=i;
+	}	
+
+}
+*/
+
+
+template<typename T>
+void JoinBufferInstances(T* a_, int stridea_,  T* b_, int strideb_, T* c)
+{
+	
+	size_t bytes = sizeof(a_[0]);
+	size_t sizea = sizeof(a_)/bytes;
+	size_t sizeb = sizeof(b_)/bytes;
+	size_t stride = stridea_+ strideb_;
+	size_t vertexnum = sizea/stridea_;
+	
+	
+	if(sizea%stridea_ && sizeb%strideb_  )
+	{
+		printf("[ %s : %s ] Error, instance vertex is not a multiple of the whole instance array.", __FILE__, __LINE__  );
+		
+	}
+	if(sizea/stridea_ != sizeb/stridea_  )
+	{
+		printf("[ %s : %s ] Error, instance array of different sizes.", __FILE__, __LINE__  );
+		
+	}
+	
+	if ( sizeof(c)/bytes != vertexnum*stride  )
+	{
+		printf("[ %s : %s ] Error, output array allocated with wrong size.", __FILE__, __LINE__  );
+	}
+	
+	for (int i=0; i<vertexnum; i++)
+	{
+		memcpy( c[i*stride], a_[i*stridea_], bytes*stridea_ );
+		memcpy( c[i*stride + stridea_], b_[i*strideb_], bytes*stridea_ );
+	}
+    	
+	
+}
+
+
